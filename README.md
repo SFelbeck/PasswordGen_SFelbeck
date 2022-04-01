0536b860e06191683f1c1c1c85ad76eba77daed8
SFelbeck <stefanfelbeck@gmail.com>
2022-03-31 17:41:51 -0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PasswordGen_SFelbeck
+
+For this assignment I was tasked with writing code in the javascript file to generate randomized passwords based on inputs from the user.
+I started this project in a semi unconvetional way in which is set up the basic framework of the function and debugged backwords from there.
+The reason I approached the project this way was that I began with a general understanding of what I needed to do and wanted to maintain the necessary elements of the code to reference while I'm still getting used to javascript syntax.
+
+With advice from classmates I was able to streamline several aspects of the project that I had a bunch of unnecessary extra code for. Such as my primary if statement to determine strings used, which was originally over 10 ifs long before I made lowercase letters default.
+
+below you can find screenshots of the beginning of the function as well as the end result.
+
+![screenshot](./assets/assets/Start.png)
+![screenshot](./assets/assets/Complete.png)
+
+<a href="https://sfelbeck.github.io/PasswordGen_SFelbeck/">You can find the site here</a>
--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 function generatePassword() {
-  
+  // declares strings that will be used as character values
   var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
   var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
   var special = "!@,=+-`~#$%&*?";
   var numbers = "1234567890"
 
+  // Initial prompt determining password length values
   let passLength = prompt("How long would you like your password? (min 8, max 20)");
   
-  var passArray = "";
+  // blank variables that will store final strings and final randomized password
+  var passString = "";
   var results = "";
   
+  // While loop that will ask user for a password length until valid value is given
   while(passLength < 8 || passLength > 20){
     alert = "Invalid password length"
     passLength = prompt ("How long would you like your password? (min 8, max 20)");
   }
 
+  // confirm events that determine if password includes uppercase of special characters
   var chooseUpper = confirm("Include uppercase characters?");
   var chooseSpecial = confirm("Include special characters?");
 
+  // if statement that determines what kind of string will be used for result with lowercase letters used as a default
   if (chooseUpper && chooseSpecial) {
-    passArray += lowerCase.concat(upperCase, special, numbers);
-    console.log(passArray);
+    passString += lowerCase.concat(upperCase, special, numbers);
   } else if (!chooseUpper && !chooseSpecial) {
-    passArray += lowerCase;
-    console.log(passArray);
+    passString += lowerCase;
   } else if (chooseUpper && !chooseSpecial) {
-    passArray += lowerCase.concat(upperCase);
-    console.log(passArray);
+    passString += lowerCase.concat(upperCase);
   }else if (!chooseUpper && chooseSpecial) {
-    passArray += lowerCase.concat(special, numbers);
-    console.log(passArray);
+    passString += lowerCase.concat(special, numbers);
   }else {
     results = ("invalid password values")
   }
 
+  // for loop that randomizes each character until total number of characters used is equal to passLength
   for(var i = 0; i < passLength; i++){
-    results += passArray.charAt(Math.floor(Math.random() * passArray.length));
+    results += passString.charAt(Math.floor(Math.random() * passString.length));
   }
-  console.log(results);
 
+  // returns results for display
   return results;
 }
 
